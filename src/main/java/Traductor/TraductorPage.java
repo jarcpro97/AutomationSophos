0c56e350c11cbed4a0a5f53d396525f9b112e866
jrodriguezcal <jrodriguez@carecloud.com.co>
2021-01-28 01:32:45 -0500

    ajustado

---
+++ src/main/java/Traductor/TraductorPage.java
@@ -0,0 +1,28 @@
+package Traductor;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class TraductorPage {
+    WebDriver driver;
+    By fieldTranslate = By.xpath("//textarea[@aria-label='Texto original']");
+    By result = By.xpath("//span[@jsname='W297wb']");
+
+    public TraductorPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void writeWord(String word) {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(fieldTranslate));
+        driver.findElement(fieldTranslate).sendKeys(word);
+    }
+
+    public String translatedWord() {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(result));
+        return driver.findElement(result).getText();
+    }
+}
